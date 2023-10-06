6cb7a88d6529464e3d2de106ac295193e0fcfe87
Subhadip023e <you@example.com>
2023-10-06 22:14:24 +0530

    nodel js

---
+++ Project/2.2+Native+Modules/2.2 Native Modules/index.js
@@ -0,0 +1,11 @@
+const fs=require("fs");
+ 
+fs.writeFile("massge2.txt","Hellow from node js ",(err) => {
+    if(err) throw err;
+    console.log("The file Has been saved!");
+ });
+
+ fs.readFile("massge2.txt", "utf8", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  });
