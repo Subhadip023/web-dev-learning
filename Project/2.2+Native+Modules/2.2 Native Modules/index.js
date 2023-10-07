52851faecec9c7328b70c87fe16301c90935ce94
Subhadip023e <you@example.com>
2023-10-07 08:33:09 +0530

    node js

--- Project/2.2+Native+Modules/2.2 Native Modules/index.js
+++ Project/2.2+Native+Modules/2.2 Native Modules/index.js
@@ -1,9 +1,9 @@
 const fs=require("fs");
  
-fs.writeFile("massge2.txt","Hellow from node js ",(err) => {
-    if(err) throw err;
-    console.log("The file Has been saved!");
- });
+// fs.writeFile("massge2.txt","Hellow from node js ",(err) => {
+//     if(err) throw err;
+//     console.log("The file Has been saved!");
+//  });
 
  fs.readFile("massge2.txt", "utf8", (err, data) => {
     if (err) throw err;
