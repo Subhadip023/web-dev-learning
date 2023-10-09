ed9e4ae51466473fad0e1ffe42dd0d12b8483f82
Subhadip023e <you@example.com>
2023-10-09 14:40:48 +0530

    qr generater

--- Project/2.3+NPM/2.3 NPM/index.js
+++ Project/2.3+NPM/2.3 NPM/index.js
@@ -1,5 +1,18 @@
 //Npm 
 //initialize NPM
-var generateName = require('sillyname');
-var sillyName = generateName();
-console.log(sillyName);
+// var generateName = require('sillyname');
+// var sillyName = generateName();
+// console.log(sillyName);
+
+// import superheroes from 'superheroes';
+// // const superheroes = require('superheroes');
+ 
+// superheroes.all;
+// //=> ['3-D Man', 'A-Bomb', …]
+ 
+// let name=superheroes.random();
+// //=> 'Spider-Ham'
+
+// console.log("My name is "+name+" !");
+
+// Qr code genaration
