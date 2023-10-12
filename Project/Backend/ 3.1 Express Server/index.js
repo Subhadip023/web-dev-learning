d142e11e0ececc99acd07154aabb65691334c98b
Subhadip023e <you@example.com>
2023-10-12 18:42:26 +0530

    ex

--- Project/Backend/ 3.1 Express Server/index.js
+++ Project/Backend/ 3.1 Express Server/index.js
@@ -1,6 +1,12 @@
 import express  from "express";
 const app =express();
 
+app.get("/",(req,res)=>{
+    res.send("hello World!");
+});
+
+
+
 app.listen(8000,()=>{
 console.log("Server runing on port 8000 .");
 
