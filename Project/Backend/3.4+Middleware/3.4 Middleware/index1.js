7980678e9eb95934dae833853d0988032e2fcdc5
Subhadip023 <subhadip240420@gmail.com>
2023-10-26 11:50:22 +0530

    secrets projects

--- Project/Backend/3.4+Middleware/3.4 Middleware/index1.js
+++ Project/Backend/3.4+Middleware/3.4 Middleware/index1.js
@@ -5,7 +5,12 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
-
+function logger(req,res,next){
+  console.log('requset method',req.method);
+  console.log('requset URI',req.uri);
+next()
+}
+ app.use(logger);
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
