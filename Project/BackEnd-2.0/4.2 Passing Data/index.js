b762dd9947eaa6bed21b52212dc3a4b5d617b392
Subhadip023 <subhadip240420@gmail.com>
2024-02-11 21:24:15 +0530

    Implement bug fix for issue #123

--- Project/BackEnd-2.0/4.2 Passing Data/index.js
+++ Project/BackEnd-2.0/4.2 Passing Data/index.js
@@ -2,13 +2,19 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const numLetters = req.body["fName"].length + req.body["lName"].length;
+  console.log(numLetters);
+  res.render("index.ejs", { numberLetters: numLetters }); // Changed key to match template
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
