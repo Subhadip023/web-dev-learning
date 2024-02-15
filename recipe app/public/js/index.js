ce36b8ae26fadb6a11740420b7f7c4f3c36215cd
Subhadip023 <subhadip240420@gmail.com>
2024-02-16 01:56:15 +0530

    finished

--- recipe app/public/js/index.js
+++ recipe app/public/js/index.js
@@ -29,17 +29,61 @@ search_btn.addEventListener("click",(e)=>{
   const search=ingredientInput.value;
   fetchRecipes(search);
 });
-closed_recipeDetails.addEventListener("click",()=>{
-  recipe_details.style.display="none";
 
+ingredientInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    const search = ingredientInput.value;
+    fetchRecipes(search);
+  }
 });
+
+
+
+document.addEventListener("click", function(event) {
+  if (event.target && event.target.id === "closed_recipeDetails") {
+      recipe_details.style.display = "none";
+  }
+});
+
 
 const getDetails=async(id)=>{
   const data=await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
   let meal=await data.json();
   meal=meal.meals[0];
-  console.log(typeof meal)
-  console.log( meal.strInstructions)
-  console.log( meal.strIngredient1)
+  var ingredientList='';
+  for(let i=1;i<=20;i++){
+    let ingrdient=meal[`strIngredient${i}`];
+    console.log(ingrdient);
+      if(ingrdient){
+        let mesure =meal[`strMeasure${i}`];
+        console.log(mesure);
+        ingredientList+=`<li>${mesure} ${ingrdient}</li>`
+      }
+      else{
+        break;
+      }
+  }
+  console.log( ingredientList)
   recipe_details.style.display="flex";
+  recipe_details.innerHTML=`
+  <div class="details">
+  <h1>${meal.strMeal}</h1>
+  <img src=${meal.strMealThumb} alt="">
+  <h2>Instructions: </h2>
+  <p>${meal.strInstructions}</p> 
+  <h2>
+    Ingredients:
+  </h2>  
+  <ol>
+  ${ingredientList}
+  </ol>
+  <h3>
+  <a href=${meal.strYoutube}>YouTube</a> 
+  </h3>
+  </div>
+  <svg id="closed_recipeDetails" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="#FBFADA">
+  <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/>
+</svg>
+  `;
 }
