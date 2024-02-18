460bc60bdc07e8ba9eaac92ac455b3a6001f2d3b
Subhadip023 <subhadip240420@gmail.com>
2024-02-18 23:39:53 +0530

    sql 2

--- Project/db/8.2 Postgres Read/index.js
+++ Project/db/8.2 Postgres Read/index.js
@@ -1,21 +1,48 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "subhadip@23",
+  port: 5432,
+});
 
 const app = express();
 const port = 3000;
-
-let totalCorrect = 0;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let quiz = [];
+let totalCorrect = 0;
 let currentQuestion = {};
 
+async function initializeQuiz() {
+  await db.connect();
+  try {
+    const result = await db.query("SELECT * FROM flags");
+    quiz = result.rows;
+  } catch (error) {
+    console.error("Error executing query", error.stack);
+  } finally {
+    await db.end();
+  }
+}
+
+async function nextQuestion() {
+  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+  currentQuestion = randomCountry;
+}
+
 // GET home page
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   totalCorrect = 0;
-  nextQuestion();
+  await initializeQuiz();
+  await nextQuestion();
   console.log(currentQuestion);
   res.render("index.ejs", { question: currentQuestion });
 });
@@ -24,7 +51,7 @@ app.get("/", (req, res) => {
 app.post("/submit", (req, res) => {
   let answer = req.body.answer.trim();
   let isCorrect = false;
-  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+  if (currentQuestion.name.toLowerCase() === answer.toLowerCase()) {
     totalCorrect++;
     console.log(totalCorrect);
     isCorrect = true;
@@ -37,11 +64,6 @@ app.post("/submit", (req, res) => {
     totalScore: totalCorrect,
   });
 });
-
-function nextQuestion() {
-  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
-  currentQuestion = randomCountry;
-}
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
