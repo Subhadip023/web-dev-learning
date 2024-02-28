c711e4a441442e5046cd5d05a8b74c05e8d71ce3
Subhadip023 <subhadip240420@gmail.com>
2024-02-28 12:15:29 +0530

    add tolower case

--- Project/react/intro-jsx/src/components/TextFrom.js
+++ Project/react/intro-jsx/src/components/TextFrom.js
@@ -6,17 +6,34 @@ export default function TextForm(props) {
     
 
   }
+  const handleLoClick =()=>{
+    let newText=text.toLowerCase()
+    setText(newText)
+    
+
+  }
   const handlenoChange =(event)=>{
     setText(event.target.value)
   }
   const [text, setText] = useState("Enter text here");
   return (
-    <div>
+    <>
+    <div className="container">
       <h1>{props.heading}</h1>
       <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handlenoChange} id="myBox" rows="8"></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary mx-3" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary mx-3" onClick={handleLoClick}>Convert to Lowercase</button>
     </div>
+<div className="container my-3">
+<h2>Your text summery </h2>
+<p>{text.split(" ").length} worlds ,{text.length} charecters </p>
+<p>{0.008*text.split(" ").length} mint read</p>
+<h3>Preview</h3>
+<p>{text}</p>
+</div>
+
+    </>
   );
 }
