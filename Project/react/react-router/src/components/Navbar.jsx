43d56587fd9fffba5ed3d8430e4bfbcfc6c9bbb3
Subhadip023 <subhadip240420@gmail.com>
2024-03-01 01:29:10 +0530

    done react routing

--- Project/react/react-router/src/components/Navbar.jsx
+++ Project/react/react-router/src/components/Navbar.jsx
@@ -1,20 +1,17 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-export default function Navbar() {
+const Navbar = () => {
+    
   return (
     <div>
       <nav>
-        <Link to="/">
-          <li>Home</li>{" "}
-        </Link>
-        <Link to="/login">
-          <li>Login</li>{" "}
-        </Link>
-        <Link to="/about">
-          <li>Contact</li>{" "}
-        </Link>
+        <NavLink className={(e)=>{return e.isActive?"red": "" }} to="/"><li>Home</li></NavLink>
+        <NavLink className={(e)=>{return e.isActive?"red": "" }} to="/about"><li>About</li></NavLink>
+        <NavLink className={(e)=>{return e.isActive?"red": "" }} to="/login"><li>Login</li></NavLink>
       </nav>
     </div>
-  );
+  )
 }
+
+export default Navbar
