33391f1937a607505c48a72594ab77e1bcb6b251
Subhadip023 <subhadip240420@gmail.com>
2024-03-01 00:20:53 +0530

    r2.0

--- Project/react/tet-util/src/App.js
+++ Project/react/tet-util/src/App.js
@@ -1,42 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
-import TextFrom from './components/TextFrom';
-import About from './components/About';
-import { useState } from 'react';
-import Alert from './components/Alert';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextFrom from "./components/TextFrom"; // Corrected import
+import About from "./components/About";
+import { useState } from "react";
+import Alert from "./components/Alert";
 import React from "react";
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-let name = 'subhadip Chakraborty';
+let name = "subhadip Chakraborty";
 
 function App() {
-  const [Mode, setMode] = useState('light');
+  const [Mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
 
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
-    })
+      type: type,
+    });
     setTimeout(() => {
       setAlert(null);
-    }, 2000)
-  }
+    }, 2000);
+  };
 
   const toggleMode = () => {
-    if (Mode === 'light') {
-      setMode('dark');
+    if (Mode === "light") {
+      setMode("dark");
       document.body.style.backgroundColor = "black";
       document.body.style.color = "white";
       showAlert("Dark mode has been activated", "success");
     } else {
-      setMode('light');
+      setMode("light");
       document.body.style.backgroundColor = "white";
       document.body.style.color = "black";
       showAlert("Light mode has been activated", "success");
@@ -47,12 +43,22 @@ function App() {
     <>
       <Navbar title="TextUtil" mode={Mode} toggleMode={toggleMode} />
       {alert && <Alert alert={alert} />}
-      <Router>
+
+      <BrowserRouter>
         <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/" element={<TextFrom showAlert={showAlert} heading="Enter the Text to Analyze Below" />} />
+          <Route path="/about" element={<About />} /> 
+          <Route
+            path="/"
+            element={
+              
+              <TextFrom
+                showAlert={showAlert}
+                heading="Enter the Text to Analyze Below"
+              />
+            }
+          />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </>
   );
 }
