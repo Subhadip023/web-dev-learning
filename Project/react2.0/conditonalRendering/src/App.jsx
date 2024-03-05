0ae153907011d37ce3e992918dbf715368973fa7
Subhadip023 <subhadip240420@gmail.com>
2024-03-05 20:42:54 +0530

    conditional rendring

--- Project/react2.0/conditonalRendering/src/App.jsx
+++ Project/react2.0/conditonalRendering/src/App.jsx
@@ -1,17 +1,12 @@
-import { useEffect, useState ,useRef} from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [showbtn, setshowbtn] = useState(false)
 
- const btnref=useRef(0)
-useEffect(()=>{
-  console.log(`Frist Rendering rendering `);
-  ref.current.style.backgroundColor='green';
-
-},[])
   return (
     <>
       <div>
@@ -23,15 +18,19 @@ useEffect(()=>{
         </a>
       </div>
       <h1>Vite + React</h1>
+      {
+        showbtn && <button>this is if showbtn is true</button>
+      }
       <div className="card">
-        <button ref={btnref} onClick={() => setCount((count) => count + 1)}>
+        {/* <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button> */}
+        <button onClick={() =>setshowbtn(!showbtn)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-        {/* <button onClick={()=>{btnref.current.style.display="none";}}>Hide Display </button> */}
-
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
