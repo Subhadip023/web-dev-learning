e265b00caddaa8652cbbbd0c7608f14870000f5b
Subhadip023 <subhadip240420@gmail.com>
2024-03-06 23:55:13 +0530

    2.0

--- Project/react2.0/handelingEvent/src/App.jsx
+++ Project/react2.0/handelingEvent/src/App.jsx
@@ -5,22 +5,25 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [from, setfrom] = useState({email:'',phone:''});
 
   const handelcicke = () => {
     alert("clicked");
   };
-  const mouseover = () => {
-    alert("clicked ,mouseover");
+  const handelin = (e) => {
+    setfrom({ ...from, [e.target.name]: e.target.value });
   };
+  
 
   return (
     <>
       <div className="button">
         <button onClick={handelcicke}>Click me </button>
       </div>
-      <div className="red" onMouseOver={mouseover}>
+      {/* <div className="red" onMouseOver={mouseover}>
         i'm red div
-      </div>
+      </div> */}
+      <input type="text" name="email" value={from.email} onChange={handelin}/>
     </>
   );
 }
